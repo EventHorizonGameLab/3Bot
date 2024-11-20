0f348136b2bac152a33197690bc076f3f3fdaf67
STRANOstudios <friand2021@gmail.com>
2024-11-20 10:59:18 +0100

    update

--- Assets/Code/Scripts/Audio/BaseAudioHandler.cs
+++ Assets/Code/Scripts/Audio/BaseAudioHandler.cs
@@ -1,30 +1,17 @@
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Audio;
 
 public class BaseAudioHandler : MonoBehaviour
 {
     [Title("Audio Settings")]
-    [SerializeField, PropertyOrder(1)] private AudioClip _audioClip;
-    [SerializeField, PropertyOrder(1)] private GameObject _audioSourcePrefab;
+    [SerializeField, PropertyOrder(1)] private string _audioClipName;
 
     /// <summary>
     /// Plays the audio clip and returns the prefab to the pool after completion.
     /// </summary>
     protected virtual void Play()
     {
-        // Ottieni il prefab dall'ObjectPooler
-        var audioSourceObject = ObjectPooler.Instance.Get(_audioSourcePrefab);
-        
-        if (!audioSourceObject.TryGetComponent<AudioSource>(out var audioSource))
-        {
-            Debug.LogError("AudioSource component missing from the prefab.");
-            return;
-        }
-
-        // Imposta la clip audio
-        audioSource.clip = _audioClip;
-
-        // Avvia la riproduzione
-        audioSource.Play();
+        AudioManager.Instance.PlaySFX(_audioClipName, transform);
     }
 }
