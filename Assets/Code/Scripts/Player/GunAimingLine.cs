997d834a04dacecdf73a107996e449cca333b574
STRANOstudios <friand2021@gmail.com>
2024-11-28 20:41:28 +0100

    update

--- Assets/Code/Scripts/Player/GunAimingLine.cs
+++ Assets/Code/Scripts/Player/GunAimingLine.cs
@@ -47,66 +47,35 @@ public class GunAimingLine : MonoBehaviour
     private void DrawLine()
     {
         Vector3 startPoint = _gunTip.position;
+
         Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (_drawGizmo) Debug.DrawRay(Camera.main.transform.position, mouseRay.direction * _maxDistance * 2, Color.red);
+        Plane plane = new (Vector3.up, new Vector3(0, _gunTip.position.y, 0));
 
-        // Perform the first raycast
-        endPoint = (Physics.Raycast(mouseRay, out RaycastHit firstHit, _maxDistance, ~excludeLayers))
-            ? CalculateImpactPoint(startPoint, firstHit.point)
-            : GetRayIntersectionWithPlane(mouseRay, _gunTip.position.y);
-
-        // Now cast the second detectionRay in the calculated direction or no-impact point
-        Ray secondRay = new(startPoint, (endPoint - startPoint).normalized);
-
-        if (_drawGizmo) Debug.DrawRay(startPoint, secondRay.direction * _maxDistance, Color.cyan);
-
-        if (Physics.Raycast(secondRay, out RaycastHit secondHit, _maxDistance, ~excludeLayers))
+        if (plane.Raycast(mouseRay, out float enter))
         {
-            endPoint = secondHit.point;
+            endPoint = mouseRay.GetPoint(enter);
+
+            Ray secondRay = new(startPoint, (endPoint - startPoint).normalized);
+
+            if (Physics.Raycast(secondRay, out RaycastHit secondHit, _maxDistance, ~excludeLayers))
+            {
+                endPoint = secondHit.point;
+            }
+            else
+            {
+                endPoint = startPoint + secondRay.direction * _maxDistance;
+            }
         }
         else
         {
-            endPoint = startPoint + secondRay.direction * _maxDistance;
+            endPoint = startPoint + mouseRay.direction * _maxDistance;
         }
-
-        if (_drawGizmo) Debug.DrawRay(endPoint, new Vector3(endPoint.x, -_maxDistance, endPoint.z), Color.green);
-
-        _gunTip.LookAt(endPoint);
 
         lineRenderer.SetPosition(0, startPoint);
         lineRenderer.SetPosition(1, endPoint);
-    }
 
-    private Vector3 CalculateImpactPoint(Vector3 startPoint, Vector3 firstHitPoint)
-    {
-        Vector3 direction = (firstHitPoint - startPoint).normalized;
-        if (_is2D) direction.y = 0;
-        return NoImpact(startPoint, direction);
-    }
-
-    private Vector3 NoImpact(Vector3 startPoint, Vector3 direction)
-    {
-        Ray secondRay = new(startPoint, direction);
-        if (Physics.Raycast(secondRay, out RaycastHit secondHit, _maxDistance, ~excludeLayers))
-        {
-            return secondHit.point;
-        }
-        else
-        {
-            return startPoint + direction * _maxDistance;
-        }
-    }
-
-    // Get intersection of the detectionRay with a horizontal plane at the given height (y-coordinate)
-    private Vector3 GetRayIntersectionWithPlane(Ray ray, float planeHeight)
-    {
-        if (Mathf.Approximately(ray.direction.y, 0f)) return Vector3.zero;
-
-        float t = (planeHeight - ray.origin.y) / ray.direction.y;
-        if (t < 0) return Vector3.zero;
-
-        return ray.origin + t * ray.direction;
+        _gunTip.LookAt(endPoint);
     }
 
     private void CheckState(bool state)
@@ -149,5 +118,4 @@ public class GunAimingLine : MonoBehaviour
     }
 
     #endregion
-
 }
