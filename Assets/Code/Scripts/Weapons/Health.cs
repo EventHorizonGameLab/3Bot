bb292d0afe3a81d04e439f9e9779977f16749832
STRANOstudios <friand2021@gmail.com>
2024-11-28 12:41:40 +0100

    update
    - activate e disattivate kinematic player

--- Assets/Code/Scripts/Weapons/Health.cs
+++ Assets/Code/Scripts/Weapons/Health.cs
@@ -48,11 +48,17 @@ public class Health : MonoBehaviour, IExplosionAffected, ITakeDamage
     public static event Action<AttackType> OnTakeDamage;
     public static event Action OnDeath;
 
+    private Rigidbody _rigidbody;
+
     private void Start()
     {
         _currentHealth = _maxHealth;
 
-        if (_isPlayer) OnHealthChange?.Invoke(_currentHealth);
+        if (_isPlayer)
+        {
+            OnHealthChange?.Invoke(_currentHealth);
+            _rigidbody = GetComponent<Rigidbody>();
+        }
     }
     private void OnEnable()
     {
@@ -157,6 +163,20 @@ public class Health : MonoBehaviour, IExplosionAffected, ITakeDamage
     public void TakeDamage(float damage, AttackType type)
     {
         ApplyDamage((int)damage, type);
+
+        if (type == AttackType.Explosive && _isPlayer && _rigidbody.isKinematic == true)
+        {
+            _rigidbody.isKinematic = false;
+            StartCoroutine(ReEnableKinematic());
+        }
+    }
+
+    private IEnumerator ReEnableKinematic()
+    {
+        if (_debug) Debug.Log("Re-enabling kinematic");
+
+        yield return new WaitForSeconds(0.5f);
+        _rigidbody.isKinematic = true;
     }
 
     public float health
